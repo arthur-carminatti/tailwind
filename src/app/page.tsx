46b165ce5335f9cfa57451b5c0f5f21aa25f0cb2
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-08-22 21:28:15 -0300

    Colors and spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <h1>Hello Tailwind</h1>
+    <div className="p-4 bg-slate-900 text-slate-100 h-screen">
+      <h1 className="font-bold text-5xl">Hello Tailwind</h1>
+      <h1 className="">Hello Tailwind</h1>
+    </div>
   )
 }
